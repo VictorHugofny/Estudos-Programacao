b0397596a4943ae17383d46e4e56eedfffafa957
victorhugofny <victorhugofny@gmail>
2021-01-11 23:48:03 -0300

    main

--- JavaScript/introdução/aritmeticos.js
+++ JavaScript/introdução/aritmeticos.js
@@ -1,3 +1,9 @@
 const[a,b,c,d] = [3,5,1,15]
 const soma = a + b + c + d
-console.log(soma)
+const subtracao = d - b
+const multipli = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log (soma,subtracao,multipli,divisao,modulo)
+
