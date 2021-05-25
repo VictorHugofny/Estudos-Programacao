26ff2e8ad088fa000ab0fe46e466a1d648fb48e1
Victor Hugo <victorhugofny@gmail.com>
2021-05-25 15:06:05 -0300

    att

--- Html/exercicios/form/server.js
+++ Html/exercicios/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 app.post('/usuarios',(req,resp) =>{
     console.log(req.body)
-    resp.send('Parabens')
+    resp.send('Parabens usuario incluido')
+})
+
+app.post('/usuarios/:id',(req,resp) =>{
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('Parabens usuario alterado')
 })
 
 app.listen(3003)
