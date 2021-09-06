9702c29ea592383ac001fecac2f328eead7297cf
Victor Hugo <victorhugofny@gmail.com>
2021-09-06 09:02:32 -0300

    Create teste.js

---
+++ JavaScript/atividades/fundament,estrutura,func.js/teste.js
@@ -0,0 +1,13 @@
+function equacao (a,b,escolha){
+    if (escolha == "soma"){
+    console.log("soma", a + b)
+    }
+    else if (escolha == "subtrair"){
+        console.log("subtrair", a - b)
+    }
+    else{
+        console.log("vc informou algo invalido")
+    }
+}
+equacao(4,4,"eaee")
+
