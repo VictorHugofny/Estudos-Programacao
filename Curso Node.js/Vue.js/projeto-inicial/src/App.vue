9cb26296ada7759fa8826ef071ed436219ecb839
Victor Hugo <victorhugofny@gmail.com>
2021-10-21 15:49:13 -0300

    estudos Vue.js

--- Curso Node.js/Vue.js/projeto-inicial/src/App.vue
+++ Curso Node.js/Vue.js/projeto-inicial/src/App.vue
@@ -1,29 +1,56 @@
 <template>
   <div id="app">
+    <h3>Cadastro </h3>
+    <hr>
+    <input type="text" placeholder="nome" v-model = "nomeField">
+    <input type="email" placeholder="email" v-model = "emailField">
+    <input type="number" placeholder="idade" v-model = "idadeField">
+    <button @click="cadastrarUsuario"> CADASTRAR </button>
+    <hr>
    <h1> Guia Clientes </h1>
-   <input type="text" v-model = "nomeDoVictor">
-   <Client :nome = "nomeDoVictor" email ="victorhugofny@gmail.com" idade = "20"/>
-   <Client nome = "Victor" email ="dualviciobrs@gmail.com" idade = "22"/>
-   <Client nome ="Funny" email ="hugoheadshot@gmail.com" idade = "20"/>
-   <Client nome = "Teste" email ="testando@gmail.com" idade = "22"/>
-   <Produto/>
+
+   
+  <div v-for = "(cliente,index) in clientes" :key = "cliente.id">
+  <h3>{{index + 1}} </h3>
+  <Client :cliente="cliente"/>
+  <hr>
+  </div>
+
   </div>
 </template>
 
 <script>
 import Client from './components/Client.vue'
-import Produto from './components/Produto.vue'
 
 export default {
   name: 'App',
   data(){
     return {
-      nomeDoVictor : "Victor Hugo"
+      nomeField : '',
+      emailField : '',
+      idadeField : '',
+      clientes : [
+        {
+          id : 1,
+          nome : "Victor Hugo",
+          email: "victorhugofny@gmail.com",
+          idade: 22
+        }
+      ]
     }
   },
   components:{
     Client,
-    Produto
+    //Produto
+  },
+  methods: {
+    cadastrarUsuario: function(){
+      this.clientes.push({nome: this.nomeField, email: this.emailField, idade: this.idadeField, id: Date.now()})
+      this.nomeField = '';
+      this.emailField = '';
+      this.idadeField = '';
+
+    }
   }
 }
 </script>
--- Curso Node.js/Vue.js/projeto-inicial/src/components/Client.vue
+++ Curso Node.js/Vue.js/projeto-inicial/src/components/Client.vue
@@ -1,10 +1,12 @@
 <template>
-<div id = "cliente">
-  <h4> Nome: {{nome}} </h4>
+<div :class = "{'cliente': !insPremium, 'cliente-premium': insPremium}">
+  <h4> Nome: {{cliente.nome}} </h4>
   <hr>
-  <p>Email: {{email}} </p>
+  <p>Email: {{cliente.email}} </p>
   <hr>
-  <p>Idade: {{idade}} </p>
+  <p v-if = "showIdade == true">Idade: {{cliente.idade}} </p>
+  <p v-else> Idade escondida </p>
+  <button @click="mudarCor($event)" > MUDAR COR </button>
 </div>
 </template>
 
@@ -12,21 +14,36 @@
 export default {
     data(){
         return {
-          
+            insPremium : false
         }
     },
     props:{
-        nome: String,
-        email: String,
-        idade : Number
+        cliente: Object,
+        showIdade: Boolean
+    },
+    methods:{
+        mudarCor: function($event){
+            console.log($event)
+            this.insPremium = !this.insPremium
+        }
     }
 }
 </script>
 
 <style>
-#cliente{
+.cliente{
     color: rgb(0, 0, 0);
     background-color: rgb(221, 219, 219);
+    max-width: 600px;
+    border: aqua solid 3px;
+    height: 200px;
+    padding-left: 2%;
+    margin-top: 2%;
+}
+
+.cliente-premium{
+    color: rgb(255, 230, 0);
+    background-color: rgb(0, 0, 0);
     max-width: 600px;
     border: aqua solid 3px;
     height: 200px;
