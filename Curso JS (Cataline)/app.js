345606bf7e6118d4c82b4da09982925cb635c11f
Victor Hugo <victorhugofny@gmail.com>
2021-12-07 21:54:21 -0300

    inicio dos estudos

---
+++ Curso JS (Cataline)/app.js
@@ -0,0 +1,6 @@
+let voceComprouProdutos = new Promise(function (resolve,reject){ //resolução e rejeição
+    let produtos = ['camisa','sapato','calça']
+    resolve('deu ruim')
+})
+
+console.log(voceComprouProdutos)
