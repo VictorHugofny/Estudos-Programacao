f0fd514e392ac8f35f309a4620ec076ff099fc1a
Victor Hugo <victorhugofny@gmail.com>
2021-12-08 22:39:36 -0300

    Update app.js

--- Curso JS (Cataline)/ajax/app.js
+++ Curso JS (Cataline)/ajax/app.js
@@ -1,5 +1,15 @@
 var button = document.querySelector('#app button')
 
 button.addEventListener('click',function(){
-    
+    var xhr = new XMLHttpRequest()
+
+    xhr.open('GET','https://api.github.com/users')
+    xhr.send()
+
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4){
+        let data = JSON.parse(xhr.responseText)
+        console.log(data[0].login)
+        }
+    }
 })
