ffd9bbb1cc9edf8ee7fa43a2d9826855367b612a
Victor Hugo <victorhugofny@gmail.com>
2022-01-18 22:23:40 -0300

    estudando API

--- Curso Web Moderno/JavaScript/Node/projeto/src/servidor.js
+++ Curso Web Moderno/JavaScript/Node/projeto/src/servidor.js
@@ -2,17 +2,22 @@ const porta = 3001
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDados')
 
 app.get('/produtos',(req,res,next)=>{
-    console.log("Middley1..")
-    next() // ir para a proxima função
+    res.send(bancoDeDados.getProdutos()) //converter para JSON
 })
 
-app.get('/produtos',(req,res,next)=>{
-    res.send({nome:'Notebook',Preco: 123.45}) //converter para JSON
+app.get('/produtos/:id',(req, res,next)=>{
+    res.send(bancoDeDados.getProduto(req.params.id))
 })
-
-
+app.post('/produtos',(req,res,next)=>{
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    
+})
 
 app.listen(porta,()=>{
     console.log('Servidor esta executando na porta ' + porta)
