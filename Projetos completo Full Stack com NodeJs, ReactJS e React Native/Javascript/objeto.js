0f0263182f36be709f4c84897672edbbb2235456
Victor Hugo <victorhugofny@gmail.com>
2022-08-11 22:49:53 -0300

    Create objeto.js

---
+++ Projetos completo Full Stack com NodeJs, ReactJS e React Native/Javascript/objeto.js
@@ -0,0 +1,15 @@
+let pessoa = [{
+    nome:'Hugo',
+    idade:22
+},
+{
+    nome:'teste',
+    idade:22
+},
+{
+    nome:'Hugo',
+    idade:22
+},
+] 
+pessoa[0].nome = "objeto"
+console.log(pessoa)
