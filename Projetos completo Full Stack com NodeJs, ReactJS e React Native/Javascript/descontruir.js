34eb200099b577e94d9436863fa834e3106e7e3a
Victor Hugo <victorhugofny@gmail.com>
2022-08-11 23:16:09 -0300

    Update descontruir.js

--- Projetos completo Full Stack com NodeJs, ReactJS e React Native/Javascript/descontruir.js
+++ Projetos completo Full Stack com NodeJs, ReactJS e React Native/Javascript/descontruir.js
@@ -3,5 +3,16 @@ let pessoa = {
     idade:'22'
 }
 
+//descontruir objetos
+
 const {nome, idade:idadeNovo} = pessoa;
 console.log(nome, idadeNovo)
+
+//descontruir array
+let nomes = ['hugo','victor','teste']
+
+const {0:hugo, 1:ceret} = nomes
+console.log(ceret)
+
+let [primeiroNome] = nomes;
+console.log(primeiroNome)
