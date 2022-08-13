e0a9795afa10e41f7a2e589ff16d965f5c511dfe
Victor Hugo <victorhugofny@gmail.com>
2022-08-13 00:34:13 -0300

    Update App.js

--- Full Stack NodeJs, ReactJS e React Native/react/my-app/src/App.js
+++ Full Stack NodeJs, ReactJS e React Native/react/my-app/src/App.js
@@ -1,11 +1,21 @@
 import Nome from './componentes/nome';
+import { useState } from 'react'
 
 function App(){
+
+  const [aluno, setAluno] = useState('Programador');
+
+  function mudarNome(nome){
+    setAluno(nome)
+  }
+
   return(
     <div>
       <h1>"teste"</h1>
+      <h1>teste </h1>
+      <h2>Ola: {aluno} </h2>
+      <button onClick= {() => mudarNome('hugo')}> Mudar nome</button>
       <Nome aluno="hugo" idade = "22"/>
-      <Nome aluno = "victor" idade = "22"/>
     </div>
   );
 }
