f139f2728a48849b635ee5412d0bba5d955ac673
Victor Hugo <victorhugofny@gmail.com>
2022-08-13 01:23:33 -0300

    react usando useState

--- Full Stack NodeJs, ReactJS e React Native/react/my-app/src/App.js
+++ Full Stack NodeJs, ReactJS e React Native/react/my-app/src/App.js
@@ -2,21 +2,51 @@ import Nome from './componentes/nome';
 import { useState } from 'react'
 
 function App(){
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState('');
 
-  const [aluno, setAluno] = useState('Programador');
+  const [user, setUser] = useState({});
 
-  function mudarNome(nome){
-    setAluno(nome)
+  function registrar(e){
+    //evitar que atualize
+    e.preventDefault();
+
+    alert('Usuario registrado com sucesso')
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email
+    })
   }
 
   return(
+   <div>
+      <form onSubmit = {registrar}>
+        <label>Nome:</label> <br/>
+        <input placeholder='digite seu nome' 
+        value={nome} onChange = {(e)=> setNome(e.target.value)}/> <br/>
+
+        <label>idade:</label> <br/>
+        <input placeholder='digite sua idade' 
+        value={idade} onChange = {(e)=> setIdade(e.target.value)}/> <br/>
+
+        <label>email:</label> <br/>
+        <input placeholder='digite seu email' 
+        value={email} onChange = {(e)=> setEmail(e.target.value)}/> <br/>
+        
+        <br/>
+        <button type="submit">Registrar</button>
+      </form>
+
+      <br/><br/>
+   
+   
     <div>
-      <h1>"teste"</h1>
-      <h1>teste </h1>
-      <h2>Ola: {aluno} </h2>
-      <button onClick= {() => mudarNome('hugo')}> Mudar nome</button>
-      <Nome aluno="hugo" idade = "22"/>
+      <span> Bem vindo: {user.nome} de idade {user.idade} de email {user.email}</span>
     </div>
+   </div>
+
   );
 }
 
