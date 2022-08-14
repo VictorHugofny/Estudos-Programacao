2c76b4fd03ed029c151149a0630621e9ed9f21fd
Victor Hugo <victorhugofny@gmail.com>
2022-08-13 21:07:21 -0300

    react estudos

--- Full Stack NodeJs, ReactJS e React Native/react/my-app/src/App.js
+++ Full Stack NodeJs, ReactJS e React Native/react/my-app/src/App.js
@@ -2,9 +2,9 @@ import Nome from './componentes/nome';
 import { useState } from 'react'
 
 function App(){
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [idade, setIdade] = useState('');
+  const [input, setInput] = useState('');
+  const [tarefas, settarefas] = useState([]);
+
 
   const [user, setUser] = useState({});
 
@@ -13,27 +13,18 @@ function App(){
     e.preventDefault();
 
     alert('Usuario registrado com sucesso')
-    setUser({
-      nome: nome,
-      idade: idade,
-      email: email
-    })
+    settarefas([...tarefas,input])
+    setInput('')
   }
 
   return(
    <div>
       <form onSubmit = {registrar}>
-        <label>Nome:</label> <br/>
-        <input placeholder='digite seu nome' 
-        value={nome} onChange = {(e)=> setNome(e.target.value)}/> <br/>
+        <label>Nome da tarefa:</label> <br/>
+        <input placeholder='digite uma tareff' 
+        value={input} onChange = {(e)=> setInput(e.target.value)}/> <br/>
 
-        <label>idade:</label> <br/>
-        <input placeholder='digite sua idade' 
-        value={idade} onChange = {(e)=> setIdade(e.target.value)}/> <br/>
-
-        <label>email:</label> <br/>
-        <input placeholder='digite seu email' 
-        value={email} onChange = {(e)=> setEmail(e.target.value)}/> <br/>
+       
         
         <br/>
         <button type="submit">Registrar</button>
@@ -42,9 +33,11 @@ function App(){
       <br/><br/>
    
    
-    <div>
-      <span> Bem vindo: {user.nome} de idade {user.idade} de email {user.email}</span>
-    </div>
+    <ul>
+      {tarefas.map( obj => (
+        <li key={tarefas}> {obj} </li> 
+        ))}
+    </ul>
    </div>
 
   );
