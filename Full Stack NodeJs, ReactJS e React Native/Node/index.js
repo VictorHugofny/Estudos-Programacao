bbb06683c675b0b5a890edf47346be55f1c671a5
Victor Hugo <victorhugofny@gmail.com>
2022-08-24 16:23:04 -0300

    Update index.js

--- Full Stack NodeJs, ReactJS e React Native/Node/index.js
+++ Full Stack NodeJs, ReactJS e React Native/Node/index.js
@@ -3,11 +3,19 @@ const express = require('express');
 
 const server = express();
 
+//query params = ?nome=nodejs
+
+//route params = /curso/teste
+
+// request body = {nome: 'teste', tipo: 'testando'}
+
 //localhost:3000/curso
 //dados recebido da requisição
 //dados enviados como resposta
-server.get('/curso',(req, res)=>{
-    return res.json({curso:'json'})
+server.get('/curso/:id',(req, res)=>{
+    const id = req.params.id;
+    const nome = req.query.nome;
+    return res.json({curso:'json ' + id})
 })
 
 server.listen(3000);
