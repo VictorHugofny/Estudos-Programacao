843ea37c71307dd6680d48a95024fdfad32c3676
Victor Hugo <victorhugofny@gmail.com>
2023-01-02 13:43:16 -0300

    backend

--- Full Stack NodeJs, ReactJS e React Native/Projeto/backend/src/routes.ts
+++ Full Stack NodeJs, ReactJS e React Native/Projeto/backend/src/routes.ts
@@ -3,7 +3,8 @@ import {Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/teste',(req: Request, res: Response)=>{
-    return res.json({ok: true})
+    //return res.json({ok: true})
+    throw new Error("Erro ao fazer essa requisição")
 })
 
 export {router};
