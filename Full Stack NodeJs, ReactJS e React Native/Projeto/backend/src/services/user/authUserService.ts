eff254ba20c75596df986d40c372e006c151c8f9
Victor Hugo <victorhugofny@gmail.com>
2023-01-05 17:45:57 -0300

    estudos backend

--- Full Stack NodeJs, ReactJS e React Native/Projeto/backend/src/services/user/authUserService.ts
+++ Full Stack NodeJs, ReactJS e React Native/Projeto/backend/src/services/user/authUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from '../../prisma';
 import {compare} from 'bcryptjs';
+import {sign} from 'jsonwebtoken';
 
 interface AuthRequest{
     email: string,
@@ -26,7 +27,26 @@ class AuthUserService{
             throw new Error("Senha incorreta!")
         }
 
-        return {ok: true}
+        //gerar um token jwt e devolver os dados do usuario com id, name, email
+        //se deu tudo certo, gerar token
+        const token = sign(
+        //payload dados passados para o frontend    
+        {
+            name: user.name,
+            email: user.email
+        },
+        process.env.JWT_SECRET,
+        {
+            subject: user.id,
+            expiresIn: "30d"
+        }
+        )
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
     }
 }
 
