bc279caab7952c4d2bba1677fd368a6ab28a6999
Victor Hugo <victorhugofny@gmail.com>
2023-02-21 21:16:59 -0300

    estudos

--- Full Stack NodeJs, ReactJS e React Native/Projeto/frontend/src/pages/index.tsx
+++ Full Stack NodeJs, ReactJS e React Native/Projeto/frontend/src/pages/index.tsx
@@ -1,9 +1,14 @@
-
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-     <h1>teste</h1>
+    <Head>
+      <title>Sujeito Pizza - Login</title>
+    </Head>
+    <div>
+      <h1>Sujeito pizzaria</h1>
+    </div>
     </>
   )
 }
