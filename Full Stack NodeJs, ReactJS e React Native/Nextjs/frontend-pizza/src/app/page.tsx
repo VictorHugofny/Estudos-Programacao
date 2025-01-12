53c105278c7a05068ca6c8d0cd61dc0827a3f05b
Victor Hugo Santana <victorhugofny@gmail.com>
2025-01-12 02:43:39 -0300

    implementado comunicação de login

--- Full Stack NodeJs, ReactJS e React Native/Nextjs/frontend-pizza/src/app/page.tsx
+++ Full Stack NodeJs, ReactJS e React Native/Nextjs/frontend-pizza/src/app/page.tsx
@@ -2,14 +2,58 @@ import styles from "./page.module.scss";
 import logoImg from "../../public/logo.svg";
 import Image from 'next/image'
 import Link from 'next/link'
+import { api } from "@/services/api";
+import { redirect } from "next/navigation";
+import {cookies} from 'next/headers'
 
 export default function Home() {
+
+  async function handleLogin(formData: FormData){
+    "use server"
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if(email === "" || password === ""){
+      console.log("Invalido")
+      return;
+    }
+
+    try{
+      const response = await api.post("/session",{
+        email,
+        password
+      })
+
+      if(!response.data.token){
+        return;
+      }
+      console.log(response.data);
+
+      const expressTime = 60 * 60 * 24 * 30 * 1000;
+      const cookieStore = await cookies();
+
+      cookieStore.set("session", response.data.token,{
+        maxAge: Date.now() + expressTime,
+        path: "/",
+        httpOnly: false,
+        secure: process.env.NODE_ENV === "production"
+      })
+
+    }catch(err){
+      console.log("Error: " + err)
+      return;
+    }
+
+    redirect("/dashboard");
+  }
+
   return (
     <div  className={styles.containerCenter}>
         <Image src={logoImg} alt = "Logo da Pizzaria"/>
 
         <section className={styles.login}>
-          <form>
+          <form action={handleLogin}>
             <input
               type='email'
               required
